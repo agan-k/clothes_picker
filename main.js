e3075edc70693b5d16947aaa405736eee0698e98
Koran Agan <koranagan@gmail.com>
2020-03-10 16:53:43 -0400

    clothes combinations containers and styling

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const api_key = '7ff65e290b76707972db913553d8d7da';
 
 const button = document.querySelector('button');
 const cityZipInput = document.querySelector('input');
-const weatherCondition = document.querySelector('section');
+const weatherCondition = document.querySelector('.weather');
 
 // ====== here all the clothing combinations =====
 const dressOne = document.querySelector('.one');
@@ -19,51 +19,80 @@ const dressTen = document.querySelector('.ten');
 const dressEleven = document.querySelector('.eleven');
 const dressTwelve = document.querySelector('.twelve');
 // ================================================
+// ================ landing page background =======
+const background = document.querySelector('.background');
+// ================================================
+
 button.addEventListener('click', async () => {
         const cityZip = cityZipInput.value;
         const response = await axios.get(`${weatherUrl}${cityZip}&units=imperial&appid=${api_key}`);
-        console.log(response);
-        //debugger;
+        cityZipInput.classList.add('hide');
+        button.classList.add('hide');
+        // console.log(response);
+        // debugger;
         weatherCondition.innerHTML = `
         <h3>${response.data.name}<h3>
         <p>Temperature: ${response.data.main.temp} Feels like: ${response.data.main.feels_like}</p>
         <p>Wind:  ${response.data.wind.speed}mph
-        <p>${response.data.weather[0].main} </p
-        <p>'('${response.data.weather[0].description}')'</p>
+        <p>${response.data.weather[0].main} </p>
+        
         `
         let temp = response.data.main.temp;
         let main = response.data.weather[0].main;
-        let Rain = response.data.weather[0].main.value;
-        let Snow = response.data.weather[0].main.value;
-        let Clear = response.data.weather[0].main.value;
+        // response.data.weather[0].main.value; 
 
-        if (temp < 30 && main == Snow) {
+        console.log(temp);
+        if (temp < 30 && main == 'Snow') {
+                background.classList.add('hide');
+                weatherCondition.classList.add('show');
                 dressOne.classList.add('show');
-        } else if (temp < 30 && main == Rain) {
+        } else if (temp < 30 && main == 'Rain') {
+                background.classList.add('hide');
+                weatherCondition.classList.add('show');
                 dressTwo.classList.add('show');
-        } else if (temp < 30 && main == Clear) {
+        } else if (temp < 30 && main == 'Clear') {
+                background.classList.add('hide');
+                weatherCondition.classList.add('show');
                 dressThree.classList.add('show');
 
-        } else if (temp < 50 && main == Snow) {
+        } else if (temp < 50 && main == 'Snow') {
+                background.classList.add('hide');
+                weatherCondition.classList.add('show');
                 dressFour.classList.add('show');
-        } else if (temp < 50 && main == Rain) {
+        } else if (temp < 50 && main == 'Rain') {
+                background.classList.add('hide');
+                weatherCondition.classList.add('show');
                 dressFive.classList.add('show');
-        } else if (temp < 50 && main == Clear) {
+        } else if (temp < 50 && main == 'Clear') {
+                background.classList.add('hide');
+                weatherCondition.classList.add('show');
                 dressSix.classList.add('show');
 
-        } else if (temp < 70 && main == Rain) {
+        } else if (temp < 70 && main == 'Rain') {
+                background.classList.add('hide');
+                weatherCondition.classList.add('show');
                 dressSeven.classList.add('show');
-        } else if (temp < 70 && main.value == Clear) {
+        } else if (temp < 70 && main.value == 'Clear') {
+                background.classList.add('hide');
+                weatherCondition.classList.add('show');
                 dressEight.classList.add('show');
 
-        } else if (temp < 80 && main == Rain) {
+        } else if (temp < 80 && main == 'Rain') {
+                background.classList.add('hide');
+                weatherCondition.classList.add('show');
                 dressNine.classList.add('show');
-        } else if (temp < 80 && main == Clear) {
+        } else if (temp < 80 && main == 'Clear') {
+                background.classList.add('hide');
+                weatherCondition.classList.add('show');
                 dressTen.classList.add('show');
 
-        } else if (temp > 90 && main == Rain) {
+        } else if (temp > 90 && main == 'Rain') {
+                background.classList.add('hide');
+                weatherCondition.classList.add('show');
                 dressEleven.classList.add('show');
-        } else if (temp > 90 && main == Clear) {
+        } else if (temp > 90 && main == 'Clear') {
+                background.classList.add('hide');
+                weatherCondition.classList.add('show');
                 dressTwelve.classList.add('show');
         }
 });
